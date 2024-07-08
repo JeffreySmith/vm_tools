19f8cd50a52cf667552fbed68aa5082a7b1d4f16
Jeffrey Smith <jeffrey.smith@acceldata.io>
2024-07-08 11:52:34 -0400

    Initial proof of concept

---
+++ add_users.go
@@ -0,0 +1,119 @@
+package vmtools
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Input io.Reader
+	Output io.Writer
+	indent int
+	encoder yaml.Encoder
+	Users []User
+	Header string
+	YamlString string
+}
+type Opt func(*Config)
+
+type User struct {
+	Username string `yaml:"username"`
+	Ip string `yaml:"vm_ip"`
+}
+
+type AdditionalUsers struct {
+	Users []User `yaml:"additional_users"`
+}
+
+
+func NewConfig(opts ...Opt) *Config{
+	c := &Config{
+		Input: os.Stdin,
+		Output: os.Stdout,
+		indent: 2,
+	}
+	for _, opt := range opts{
+		opt(c)
+	}
+	return c
+}
+
+func SetIndent(indent int) func(*Config) {
+	return func(c *Config){
+		c.indent = indent
+	}
+}
+
+func WithInput(buf io.Reader) func(*Config) {
+	return func(c *Config){
+		c.Input = buf
+	}
+}
+func WithOutput(buf io.Writer) func(*Config) {
+	return func(c *Config){
+		c.Output = buf
+	}
+}
+func WithHeader(header string) func(*Config){
+	return func(c *Config){
+		c.Header = header
+	}
+}
+func (c *Config) GetIndent() int {
+	return c.indent
+}
+
+func CreateUser(username string, ip string) User {
+	u := User{Username:username,Ip:ip}
+	return u
+}
+
+func (c *Config) GetUsers(ips []string) {
+	var usernames []string
+	var users []User
+	scanner := bufio.NewScanner(c.Input)
+	
+	for scanner.Scan(){
+		usernames = append(usernames,scanner.Text())
+	}
+	for _, ip := range ips {
+		for _, user := range usernames {
+			users = append(users, CreateUser(user, ip))
+		}
+	}
+	c.Users = users
+}
+func (c *Config) GenerateYaml() (string,error) {
+	var b bytes.Buffer
+	additionalusers := AdditionalUsers{Users:[]User{}}
+	for _, user := range c.Users{
+		additionalusers.Users = append(additionalusers.Users,user)
+	}
+	encoder := yaml.NewEncoder(&b)
+	defer encoder.Close()
+	encoder.SetIndent(c.indent)
+	err := encoder.Encode(&additionalusers)
+	if err != nil {
+		return "",err
+	}
+	c.YamlString = b.String()
+	
+	return c.YamlString,nil
+}
+
+func (c *Config) WriteYaml() error{
+	if len(c.YamlString) == 0 {
+		return errors.New("Uninitialized yaml string")
+	}
+	if len(c.Header) > 0 {
+		c.Output.Write([]byte(c.Header))
+		c.Output.Write([]byte{'\n'})
+	}
+	c.Output.Write([]byte(c.YamlString))
+	return nil
+}
---
+++ add_users_test.go
@@ -0,0 +1,172 @@
+package vmtools_test
+
+import (
+	//	"bytes"
+	"bufio"
+	"bytes"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/JeffreySmith/vmtools"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestIndent(t *testing.T){
+	t.Parallel()
+	conf := vmtools.NewConfig(vmtools.SetIndent(4))
+	want := 4
+	got := conf.GetIndent()
+	if want != got {
+		t.Errorf("Got %v, want %v", got, want)
+	}
+}
+func TestCreateUser(t *testing.T){
+	t.Parallel()
+	got := vmtools.CreateUser("alicebrown","10.90.9.9")
+	want := vmtools.User{Username:"alicebrown",Ip:"10.90.9.9"}
+	if want != got {
+		t.Errorf("Got %v, want %v", got, want)
+	}
+}
+
+func TestCreateMultipleUsersSingleIP(t *testing.T){
+	t.Parallel()
+	buf := strings.NewReader("bobby\nzoe")
+	config := vmtools.NewConfig(vmtools.WithInput(buf))
+	ips := []string{"10.90.9.9"}
+	config.GetUsers(ips)
+	got := config.Users
+	want := []vmtools.User{
+		{Username:"bobby",Ip:"10.90.9.9"},
+		{Username:"zoe",Ip:"10.90.9.9"},
+	}
+	if !cmp.Equal(got,want) {
+		t.Error(cmp.Diff(got,want))
+	}
+}
+
+func TestCreateMultipleUsersMultipleIPs(t *testing.T){
+	t.Parallel()
+	buf := strings.NewReader("bobby\nzoe")
+	config := vmtools.NewConfig(vmtools.WithInput(buf))
+	ips := []string{"10.90.9.9","192.168.1.4"}
+	config.GetUsers(ips)
+	got := config.Users
+	want := []vmtools.User{
+		{Username:"bobby",Ip:"10.90.9.9"},
+		{Username:"zoe",Ip:"10.90.9.9"},
+		{Username:"bobby",Ip:"192.168.1.4"},
+		{Username:"zoe",Ip:"192.168.1.4"},
+	}
+	if !cmp.Equal(got,want) {
+		t.Error(cmp.Diff(got,want))
+	}
+}
+
+func TestCreateUsersFromFile(t *testing.T){
+	t.Parallel()
+	buf,err := os.Open("testdata/users")
+	defer buf.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	config := vmtools.NewConfig(vmtools.WithInput(buf))
+	ips := []string{"10.90.9.9"}
+	config.GetUsers(ips)
+	got := config.Users
+	want := []vmtools.User{
+		{Username:"bobby",Ip:"10.90.9.9"},
+		{Username:"zoe",Ip:"10.90.9.9"},
+		{Username:"alice",Ip:"10.90.9.9"},
+		{Username:"johndoe",Ip:"10.90.9.9"},
+	}
+	if !cmp.Equal(got,want){
+		t.Error(cmp.Diff(got,want))
+	}
+}
+
+func TestYamlOutput(t *testing.T){
+	t.Parallel()
+	users := strings.NewReader("johndoe\nalice")
+
+
+	config := vmtools.NewConfig(vmtools.WithInput(users))
+	ips := []string{"10.90.9.9"}
+	config.GetUsers(ips)
+	yaml_string,err := config.GenerateYaml()
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := `additional_users:
+  - username: johndoe
+    vm_ip: 10.90.9.9
+  - username: alice
+    vm_ip: 10.90.9.9`
+	want += "\n"
+	
+	got := yaml_string
+
+	if got !=  want{
+		t.Errorf("\nGot:\n%v\nWant:\n%v", got, want)
+	}
+}
+func TestWriteYaml(t *testing.T){
+	t.Parallel()
+	var b bytes.Buffer
+	input := strings.NewReader("jupiter")
+	output := bufio.NewWriter(&b)
+	config := vmtools.NewConfig(vmtools.WithInput(input),vmtools.WithOutput(output))
+	ips := []string{"10.90.9.9"}
+	config.GetUsers(ips)
+	_,err := config.GenerateYaml()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := `additional_users:
+  - username: jupiter
+    vm_ip: 10.90.9.9`
+	want += "\n"
+
+	err = config.WriteYaml()
+	output.Flush()
+	got := b.String()
+	if err != nil {
+		t.Error(err)
+	}
+	if got != want{
+		t.Errorf("Got:\n%v, want:\n%v", got, want)
+	}
+}
+func TestWriteYamlWithHeader(t *testing.T){
+	t.Parallel()
+		var b bytes.Buffer
+	input := strings.NewReader("jupiter")
+	output := bufio.NewWriter(&b)
+	header := "#This is my header.\n#There are many like it, but this one is mine."
+	config := vmtools.NewConfig(vmtools.WithInput(input),vmtools.WithOutput(output),vmtools.WithHeader(header))
+	ips := []string{"10.90.9.9"}
+	config.GetUsers(ips)
+	_,err := config.GenerateYaml()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := `#This is my header.
+#There are many like it, but this one is mine.
+additional_users:
+  - username: jupiter
+    vm_ip: 10.90.9.9`
+	want += "\n"
+
+	err = config.WriteYaml()
+	output.Flush()
+	got := b.String()
+	if err != nil {
+		t.Error(err)
+	}
+	if got != want{
+		t.Errorf("Got:\n%v, want:\n%v", got, want)
+	}
+}
