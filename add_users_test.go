742c5266fbed89444c0868c2f558439068d547d8
Jeffrey Smith <jeffrey.smith@acceldata.io>
2024-07-08 17:01:41 -0400

    Added a few flags, ran gofmt.

--- add_users_test.go
+++ add_users_test.go
@@ -1,7 +1,6 @@
 package vmtools_test
 
 import (
-	//	"bytes"
 	"bufio"
 	"bytes"
 	"os"
@@ -12,7 +11,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestIndent(t *testing.T){
+func TestIndent(t *testing.T) {
 	t.Parallel()
 	conf := vmtools.NewConfig(vmtools.SetIndent(4))
 	want := 4
@@ -21,16 +20,16 @@ func TestIndent(t *testing.T){
 		t.Errorf("Got %v, want %v", got, want)
 	}
 }
-func TestCreateUser(t *testing.T){
+func TestCreateUser(t *testing.T) {
 	t.Parallel()
-	got := vmtools.CreateUser("alicebrown","10.90.9.9")
-	want := vmtools.User{Username:"alicebrown",Ip:"10.90.9.9"}
+	got := vmtools.CreateUser("alicebrown", "10.90.9.9")
+	want := vmtools.User{Username: "alicebrown", Ip: "10.90.9.9"}
 	if want != got {
 		t.Errorf("Got %v, want %v", got, want)
 	}
 }
 
-func TestCreateMultipleUsersSingleIP(t *testing.T){
+func TestCreateMultipleUsersSingleIP(t *testing.T) {
 	t.Parallel()
 	buf := strings.NewReader("bobby\nzoe")
 	config := vmtools.NewConfig(vmtools.WithInput(buf))
@@ -38,35 +37,35 @@ func TestCreateMultipleUsersSingleIP(t *testing.T){
 	config.GetUsers(ips)
 	got := config.Users
 	want := []vmtools.User{
-		{Username:"bobby",Ip:"10.90.9.9"},
-		{Username:"zoe",Ip:"10.90.9.9"},
+		{Username: "bobby", Ip: "10.90.9.9"},
+		{Username: "zoe", Ip: "10.90.9.9"},
 	}
-	if !cmp.Equal(got,want) {
-		t.Error(cmp.Diff(got,want))
+	if !cmp.Equal(got, want) {
+		t.Error(cmp.Diff(got, want))
 	}
 }
 
-func TestCreateMultipleUsersMultipleIPs(t *testing.T){
+func TestCreateMultipleUsersMultipleIPs(t *testing.T) {
 	t.Parallel()
 	buf := strings.NewReader("bobby\nzoe")
 	config := vmtools.NewConfig(vmtools.WithInput(buf))
-	ips := []string{"10.90.9.9","192.168.1.4"}
+	ips := []string{"10.90.9.9", "192.168.1.4"}
 	config.GetUsers(ips)
 	got := config.Users
 	want := []vmtools.User{
-		{Username:"bobby",Ip:"10.90.9.9"},
-		{Username:"zoe",Ip:"10.90.9.9"},
-		{Username:"bobby",Ip:"192.168.1.4"},
-		{Username:"zoe",Ip:"192.168.1.4"},
+		{Username: "bobby", Ip: "10.90.9.9"},
+		{Username: "zoe", Ip: "10.90.9.9"},
+		{Username: "bobby", Ip: "192.168.1.4"},
+		{Username: "zoe", Ip: "192.168.1.4"},
 	}
-	if !cmp.Equal(got,want) {
-		t.Error(cmp.Diff(got,want))
+	if !cmp.Equal(got, want) {
+		t.Error(cmp.Diff(got, want))
 	}
 }
 
-func TestCreateUsersFromFile(t *testing.T){
+func TestCreateUsersFromFile(t *testing.T) {
 	t.Parallel()
-	buf,err := os.Open("testdata/users")
+	buf, err := os.Open("testdata/users")
 	defer buf.Close()
 	if err != nil {
 		t.Fatal(err)
@@ -76,25 +75,24 @@ func TestCreateUsersFromFile(t *testing.T){
 	config.GetUsers(ips)
 	got := config.Users
 	want := []vmtools.User{
-		{Username:"bobby",Ip:"10.90.9.9"},
-		{Username:"zoe",Ip:"10.90.9.9"},
-		{Username:"alice",Ip:"10.90.9.9"},
-		{Username:"johndoe",Ip:"10.90.9.9"},
+		{Username: "bobby", Ip: "10.90.9.9"},
+		{Username: "zoe", Ip: "10.90.9.9"},
+		{Username: "alice", Ip: "10.90.9.9"},
+		{Username: "johndoe", Ip: "10.90.9.9"},
 	}
-	if !cmp.Equal(got,want){
-		t.Error(cmp.Diff(got,want))
+	if !cmp.Equal(got, want) {
+		t.Error(cmp.Diff(got, want))
 	}
 }
 
-func TestYamlOutput(t *testing.T){
+func TestYamlOutput(t *testing.T) {
 	t.Parallel()
 	users := strings.NewReader("johndoe\nalice")
-
 
 	config := vmtools.NewConfig(vmtools.WithInput(users))
 	ips := []string{"10.90.9.9"}
 	config.GetUsers(ips)
-	yaml_string,err := config.GenerateYaml()
+	yaml_string, err := config.GenerateYaml()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -104,22 +102,22 @@ func TestYamlOutput(t *testing.T){
   - username: alice
     vm_ip: 10.90.9.9`
 	want += "\n"
-	
+
 	got := yaml_string
 
-	if got !=  want{
+	if got != want {
 		t.Errorf("\nGot:\n%v\nWant:\n%v", got, want)
 	}
 }
-func TestWriteYaml(t *testing.T){
+func TestWriteYaml(t *testing.T) {
 	t.Parallel()
 	var b bytes.Buffer
 	input := strings.NewReader("jupiter")
 	output := bufio.NewWriter(&b)
-	config := vmtools.NewConfig(vmtools.WithInput(input),vmtools.WithOutput(output))
+	config := vmtools.NewConfig(vmtools.WithInput(input), vmtools.WithOutput(output))
 	ips := []string{"10.90.9.9"}
 	config.GetUsers(ips)
-	_,err := config.GenerateYaml()
+	_, err := config.GenerateYaml()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -135,20 +133,20 @@ func TestWriteYaml(t *testing.T){
 	if err != nil {
 		t.Error(err)
 	}
-	if got != want{
+	if got != want {
 		t.Errorf("Got:\n%v, want:\n%v", got, want)
 	}
 }
-func TestWriteYamlWithHeader(t *testing.T){
+func TestWriteYamlWithHeader(t *testing.T) {
 	t.Parallel()
-		var b bytes.Buffer
+	var b bytes.Buffer
 	input := strings.NewReader("jupiter")
 	output := bufio.NewWriter(&b)
 	header := "#This is my header.\n#There are many like it, but this one is mine."
-	config := vmtools.NewConfig(vmtools.WithInput(input),vmtools.WithOutput(output),vmtools.WithHeader(header))
+	config := vmtools.NewConfig(vmtools.WithInput(input), vmtools.WithOutput(output), vmtools.WithHeader(header))
 	ips := []string{"10.90.9.9"}
 	config.GetUsers(ips)
-	_,err := config.GenerateYaml()
+	_, err := config.GenerateYaml()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -166,7 +164,7 @@ additional_users:
 	if err != nil {
 		t.Error(err)
 	}
-	if got != want{
+	if got != want {
 		t.Errorf("Got:\n%v, want:\n%v", got, want)
 	}
 }
--- cmd/create_users/main.go
+++ cmd/create_users/main.go
@@ -4,10 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"io"
-
 	"os"
 	"strings"
-
 	"github.com/JeffreySmith/vmtools"
 )
 
@@ -17,11 +15,12 @@ func main() {
 	var header string
 	stdin := os.Stdin
 	f, err := stdin.Stat()
-	
+
 	ip := flag.String("ip", "", "Comma separated list of ip addresses.")
 	output := flag.String("output", "", "Output file for generated yaml.")
 	input := flag.String("input", "", "Input file for user names.")
-	header_path := flag.String("header","","Path to a file containing your yaml file header.")
+	header_path := flag.String("header", "", "Path to a file containing your yaml file header (optional).")
+	indentation_level := flag.Int("indent", 2, "Set the indentation level. Must be > 2")
 	flag.Parse()
 	if len(*ip) == 0 {
 		fmt.Fprintf(os.Stderr, "You must supply at least 1 ip address\n\n")
@@ -58,22 +57,25 @@ func main() {
 		}
 	}
 	if len(*header_path) > 0 {
-		_,err := os.Stat(*header_path)
+		_, err := os.Stat(*header_path)
 		if err != nil {
-			fmt.Fprintf(os.Stderr,"Cannot read file %v: %v\n", *header_path, err)
+			fmt.Fprintf(os.Stderr, "Cannot read file %v: %v\n", *header_path, err)
 			os.Exit(1)
 		}
-		f,err := os.ReadFile(*header_path)
+		f, err := os.ReadFile(*header_path)
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error reading %v: \n", err)
 			os.Exit(1)
 		}
 		header = string(f)
-	} 
+	} else {
+		header = "---"
+	}
 	config := vmtools.NewConfig(vmtools.WithOutput(OutputBuffer),
 		vmtools.WithInput(InputBuffer),
 		vmtools.WithHeader(header),
+		vmtools.SetIndent(*indentation_level),
 	)
 
 	config.GetUsers(ips)
